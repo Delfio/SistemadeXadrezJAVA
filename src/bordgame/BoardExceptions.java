b6b4398c30c899aa408ac86ed61c6d5c872b124a
Delfio <42546922+Delfio@users.noreply.github.com>
2019-07-22 21:14:49 -0400

    Commit22julh

---
+++ src/bordgame/BoardExceptions.java
@@ -0,0 +1,10 @@
+package bordgame;
+
+public class BoardExceptions extends RuntimeException{
+	
+	private static final long serialVersionUID = 1L;
+	
+	public BoardExceptions (String msg) {
+		super(msg);
+	}
+}
